461e4d5669cb2303387c14f0fa67ed8396286639
Alexandru Buhai <alexandru.buhai@sectorlabs.ro>
2019-10-14 09:38:39 +0300

    Add cec-utils to turn on/off the display

--- home/display-on.sh
+++ home/display-on.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo tvservice -p > /dev/null && sudo chvt 2 && sudo chvt 1 # for whatever reason, cycling virtual terminals helps wake up the display in some cases
+# Using tv
+#sudo tvservice -p > /dev/null && sudo chvt 2 && sudo chvt 1 # for whatever reason, cycling virtual terminals helps wake up the display in some cases
+
+# Using cec-client
+echo "on 0" | cec-client -s -d 1
